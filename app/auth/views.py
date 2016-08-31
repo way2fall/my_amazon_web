eb9e3ed527d1feee5bddb28bac954f2fcfb9df11
Desmond <xa_yhy@163.com>
2016-08-31 19:19:50 +0800

    finally email works

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from flask_login import login_user, logout_user, login_required, current_user
-from ..models import User, db
+from ..models import User
+from .. import db
 from .forms import LoginForm, RegistrationForm
 from ..email import send_email
 
